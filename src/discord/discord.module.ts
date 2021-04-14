be41f513f77cf335250c322b70b59edac45db2cb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-14 18:14:33 +0200

    feat: Init Config + Mongoose Module

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DiscordService } from './discord.service';
 import { DiscordController } from './discord.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: 'firstDB',
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('APP_MONGO_CONNECTION_STRING'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [DiscordController],
   providers: [DiscordService],
 })
